3ce934c6d686fb2910fd13b111add0f8219cc7b7
Oscar <Oscar@LAPTOP-GUSSUUKH>
2020-03-07 14:50:26 +0100

    Implementato Controller di base

--- src/main/java/it/polito/tdp/indovinaNumero/FXMLController.java
+++ src/main/java/it/polito/tdp/indovinaNumero/FXMLController.java
@@ -4,22 +4,55 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 
-public class FXMLController implements Initializable {
-    
+public class FXMLController {
+
     @FXML
-    private Label label;
-    
+    private ResourceBundle resources;
+
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private URL location;
+
+    @FXML
+    private Button btnNuova;
+
+    @FXML
+    private TextField txtRimasti;
+
+    @FXML
+    private HBox layoutTentativo;
+
+    @FXML
+    private TextField txtTentativi;
+
+    @FXML
+    private Button btnProva;
+
+    @FXML
+    private TextArea txtRisultato;
+
+    @FXML
+    void doNuova(ActionEvent event) {
+
     }
-    
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+
+    @FXML
+    void doTentativo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert btnNuova != null : "fx:id=\"btnNuova\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtRimasti != null : "fx:id=\"txtRimasti\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert layoutTentativo != null : "fx:id=\"layoutTentativo\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtTentativi != null : "fx:id=\"txtTentativi\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnProva != null : "fx:id=\"btnProva\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtRisultato != null : "fx:id=\"txtRisultato\" was not injected: check your FXML file 'Scene.fxml'.";
+
+    }
 }
